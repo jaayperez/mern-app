a446900c02048d5f8e1d8ddc6d0460fa50f9cfd2
Justin Perez <justin@startrev.com>
2020-03-25 18:10:48 -0700

    Update the import path for App to fix build

--- client/index.js
+++ client/index.js
@@ -1,11 +1,9 @@
 //client/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
